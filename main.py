71b56517a58ef515210c4e2d5447b98652e5d888
Fernando Candiani <79520984+FernandoRCandiani@users.noreply.github.com>
2023-03-02 19:52:19 -0300

    Create func exibir e remove ta

--- main.py
+++ main.py
@@ -1,1 +1,7 @@
-print("ta")
+dicionario = {1234:["Fernando","Candiani"]}
+
+def exibir():
+  for chave, lista in dicionario.items();
+    print("Cod. do Fucionario...", chave)
+    print("Nome.................", lista[0])
+    print("Cargo................", lista[1])
